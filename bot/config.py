6c66fd1dbc48876a2d7fc418144abef19ae664c7
theprincemission-hue <theprincemission@gmail.com>
2026-05-30 22:43:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,7 +4,7 @@ class Telegram:
     API_ID = int(env.get("TG_API_ID", 1234))
     API_HASH = env.get("TG_API_HASH", "xyz")
     BOT_TOKEN = env.get("TG_BOT_TOKEN", "abc")
-    BOT_USERNAME = env.get("TG_BOT_USERNAME", "DrReactBot")
+    BOT_USERNAME = env.get("TG_BOT_USERNAME", "reactiontelle_bot")
     EMOJIS = [
         "👍", "👎", "❤", "🔥", 
         "🥰", "👏", "😁", "🤔",
